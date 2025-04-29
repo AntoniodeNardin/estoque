84819ea66acda6d92b9790c17930c81ea30dab5d
Antonio de Nardin <antoniodenardin01@gmail.com>
2025-04-29 16:29:32 -0300

    feat: adicionando outas tabelas ao seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\Categoria;
+use App\Models\Composicao;
 use App\Models\Item;
+use App\Models\Lote;
 use App\Models\Unidade;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Usuario;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,7 +18,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Categoria::factory()->count(10)->create();
-        Item::factory()->count(20)->create();
-        Unidade::factory()->count(20)->create();
+        Item::factory()->count(10)->create();
+        Unidade::factory()->count(10)->create();
+        Composicao::factory()->count(10)->create();
+        Lote::factory()->count(10)->create();
+        Usuario::factory()->count(10)->create();
+
     }
 }
